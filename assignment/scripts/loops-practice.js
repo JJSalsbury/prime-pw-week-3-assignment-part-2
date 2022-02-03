b66d01fa2dfc96fa9c010ff3bfe7beced0d059a8
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-03 15:54:05 -0600

    loops-practice.js; completed 1a-3c; completed 3c(console checked), stretch complete

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -55,14 +55,19 @@ while (i < stars.length) {
   console.log(stars[i]);
   i++;
 }
+
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-let max=6;
-while (i < max) {
+i=0;
+while (i < 6) {
   console.log(i);
   i++;
 }
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
-
+i=10;
+while (i > 4) {
+  console.log(i);
+  i--;
+}
 //
